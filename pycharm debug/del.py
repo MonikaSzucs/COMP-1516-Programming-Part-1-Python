b5aac5c22b1daecb0c4e8254fe5d6970cfa4ea68
Monika <monika.silvia.s@hotmail.ca>
2021-09-13 13:39:17 -0700

    Adding notes on readme file and also adding test file for debugging.
    youtube.com/watch?v=sRGpvbhOhQs

---
+++ pycharm debug/del.py
@@ -0,0 +1,17 @@
+import pygame #todo import other modules
+
+pygame.init()
+test = 7
+print(test)
+print(test)
+print(test)
+
+# Changes only within scope when you refractor within the function
+
+def main(): #todo finish this function
+    tests = 0
+    print(tests)
+
+run = True
+
+run
