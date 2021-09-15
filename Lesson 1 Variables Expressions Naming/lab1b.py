849354f6fa91c0264cd7f3cb3f93805b6b8a9fa7
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-09-14 18:17:32 -0700

    lab1b done

---
+++ Lesson 1 Variables Expressions Naming/lab1b.py
@@ -0,0 +1,25 @@
+# Monika Szucs
+
+# Part A
+first_name = input("Enter your first name:")
+print("Hello, " + first_name)
+
+# Part B
+x = 5
+y = 10
+xy_sum = x + y
+print(xy_sum)
+
+# Part C
+a = 10.5
+b = 4.0
+c = a * b
+print(c)
+
+# Part D
+print(int(c) - xy_sum)
+# Question, how is the output different if you don't convert c to an int?
+# Answer: It will contain decimal places if you do not convert it to an int.
+
+# Part E
+print("This program is done.")
