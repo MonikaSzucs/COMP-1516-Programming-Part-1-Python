70fe10c393179db8c1bd7f75c658e7ee766a0f52
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-09-15 21:48:56 -0700

    Set up Lab 2 document

---
+++ Lesson 2 Functions and Modules/In-Class Lab2/retail_item.py
@@ -0,0 +1,33 @@
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+def get_retail_item_description():
+    input("enter retail item description:")
+
+
+def get_number_of_purchased_items():
+    input("enter quantity purchased:")
+
+
+def get_price_per_unit():
+    input("enter price per unit:")
+
+
+def get_tax_rate():
+    input("enter tax:")
+
+
+def calculate_subtotal(price, quantity_sold):
+    #calucate subtotal amount. Multiply price of retail item times quantity sold
+
+
+def calculate_tax(subtotal, tax):
+    #calculates and returns the tax amount of the given subtotal
+
+
+def calculate_total():
+    #Takes subtotal and tax. Returns total amount
