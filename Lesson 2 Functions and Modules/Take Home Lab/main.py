64e9e22b0a291833d81e1cb4733c35d067023730
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-09-22 21:16:34 -0700

    Set up the lab modules
    This is not done. I just set up the skeleton of the two python files.

---
+++ Lesson 2 Functions and Modules/Take Home Lab/main.py
@@ -0,0 +1,14 @@
+import utilities
+
+
+def main():
+    radius_circle = input("enter the radius of a circle in cm:",)
+    utilities.calculate_circle_area(radius_circle)
+    radius_sphere = input("enter the radius of a sphere in cm:",)
+    utilities.calculate_sphere_volume(radius_sphere)
+    utilities.calculate_BMI()
+    utilities.calculate_hypotenuse()
+
+
+if __name__ == "__main__":
+    main()
