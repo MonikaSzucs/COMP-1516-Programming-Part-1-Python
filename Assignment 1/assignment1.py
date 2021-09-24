d3f2af3f2669c07e554be81a3c0f06e51132322e
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-09-23 21:16:10 -0700

    Set up assignment 1 project

---
+++ Assignment 1/assignment1.py
@@ -0,0 +1,27 @@
+"""
+Assignment 1 by Monika Szucs, A00878763
+"""
+from data import countries_and_capitals, countries, capitals
+
+
+def print_json_countries_and_capitals():
+    pass
+
+
+def get_list_of_countries_whose_nth_letter_is(n,letter):
+
+
+
+def get_funny_case_capital_cities(letter):
+
+
+def get_doubled_letter_countries():
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ Assignment 1/main.py
@@ -0,0 +1,17 @@
+"""
+Assignment 1 by Monika Szucs, A00878763
+"""
+import assignment1
+
+
+def main():
+    print_json_countries_and_capitals()
+    print(get_list_of_countries_whose_nth_letter_is(n,letter))
+    print(get_funny_case_capital_cities(letter))
+    print(get_doubled_letter_countries())
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
