f323c5e0ef0178b5fc7ceb97fa44f63f78000b6f
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-04 22:36:40 -0700

    Set up Lab 4

---
+++ Lesson 4 Strings/Lab 4 In-Class/login.py
@@ -0,0 +1,12 @@
+def generate_login():
+    first_name = input("enter your first name: ")
+    last_name = input("enter your last name: ")
+    bcit_id = input(" enter your BCIT ID")
+    pass
+
+
+def change_password():
+    pass
+
+
+
---
+++ Lesson 4 Strings/Lab 4 Take-Home/data_format.py
@@ -0,0 +1,9 @@
+def get_book_info():
+    book_title = input("enter book title: ")
+    book_isbn = input("enter book ISBN: ")
+    book_author_last_name = input("enter author last name: ")
+    book_publisher = input("enter book publisher: ")
+    year_published = input("enter year published: ")
+    book_price = input("enter price: ")
+
+    
