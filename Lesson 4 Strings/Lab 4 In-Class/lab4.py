fde78ad34434fa6866cc911bfb8261ddc40b06de
Monika <monika.silvia.s@hotmail.ca>
2021-10-06 23:02:42 -0700

    Started Last Name
    Doesn't work yet

--- Lesson 4 Strings/Lab 4 In-Class/lab4.py
+++ Lesson 4 Strings/Lab 4 In-Class/lab4.py
@@ -2,55 +2,65 @@ import login
 
 
 def main():
-    start = "first"
-    first_name = input('enter your first name: ').split()
-    while True:
-        if start == "first":
-            first_name = ''.join(map(str, first_name))
-            first_name = first_name.title()
-            if first_name.isalpha():
-                break
-            elif len(first_name) > 1:
-                start = "incorrect spacing"
-                continue
-            else:
-                start = "incorrect"
-                continue
-        elif start == "incorrect":
-            first_name = input('Please enter only alphabetical characters for your first name: ').split()
-            while len(first_name) > 1:
-                first_name = input('Please enter only one word').split()
-                continue
-            first_name = ''.join(map(str, first_name))
-            first_name = first_name.title()
-            if first_name.isalpha():
-                break
-            else:
-                start = "correct"
-                continue
-        elif start == "incorrect spacing":
-            first_name = input('There was incorrect spacing. We only need your first name: ').split()
-            while len(first_name) > 1:
-                first_name = input('Please enter only one word').split()
-                continue
-            first_name = ''.join(map(str, first_name))
-            first_name = first_name.title()
-            if first_name.isalpha():
-                break
-            else:
-                start = "incorrect"
+    info = ["first_name", "last_name"]
+    print(len(info))
+    print(info[0])
 
-        elif name == "correct":
-            first_name = input('enter your first name: ').split()
-            first_name = ''.join(map(str, first_name))
-            first_name = first_name.title()
-            if first_name.isalpha():
-                break
+    for names in info:
+        # First Name
+        start = "first"
+        info[names] = input('enter your first name: ').split()
+        while True:
+            if start == "first":
+                info[names] = ''.join(map(str, info[names]))
+                info[names] = info[names].title()
+                if info[names].isalpha():
+                    break
+                elif len(info[names].split()) > 1:
+                    start = "incorrect spacing"
+                    continue
+                else:
+                    start = "incorrect"
+                    continue
+            elif start == "incorrect":
+                info[names] = input('Please enter only alphabetical characters for your first name: ').split()
+                while len(info[names]) > 1:
+                    info[names] = input('Please enter only one word: ').split()
+                    continue
+                info[names] = ''.join(map(str, info[names]))
+                info[names] = info[names].title()
+                if info[names].isalpha():
+                    break
+                elif len(info[names].split()) > 1:
+                    start = "incorrect spacing"
+                    continue
+                else:
+                    start = "incorrect"
+                    continue
+            elif start == "incorrect spacing":
+                info[names] = input('There was incorrect spacing. We only need your first name: ').split()
+                while len(info[names]) > 1:
+                    info[names] = input('Please enter only one word: ').split()
+                    continue
+                info[names] = ''.join(map(str, info[names]))
+                info[names] = info[names].title()
+                if info[names].isalpha():
+                    break
+                else:
+                    start = "incorrect"
 
-        print(type(first_name))
-        print(start)
+            elif start == "correct":
+                info[names] = input('enter your first name: ').split()
+                info[names] = ''.join(map(str, info[names]))
+                info[names] = info[names].title()
+                if info[names].isalpha():
+                    break
 
-    print(first_name)
+            print(type(info[names]))
+            print(start)
+
+        print(info[0])
+
 
 
 
