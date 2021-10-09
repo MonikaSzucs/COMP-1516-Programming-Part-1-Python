5b74f8ce9c640f954f330f795e3abb6ecc9eaad2
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-08 22:46:12 -0700

    Space added

--- Lesson 4 Strings/Lab 4 In-Class/login.py
+++ Lesson 4 Strings/Lab 4 In-Class/login.py
@@ -7,7 +7,7 @@ def generate_login(first_name, last_name, bcit_id):
 def change_password():
     new_password = input("enter a new password. The password should be at least 7 characters long has one uppercase, "
                          "one lowercase character and one number. It cannot have special characters")
-    
+
     pass
 
 
