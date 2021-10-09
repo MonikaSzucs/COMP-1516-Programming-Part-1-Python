c65f4bc7b827f5ef328204b40b33d265da034df7
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-08 22:40:30 -0700

    Firstname, lastname and BCIT ID check done
    Finished logic using arrays and made sure i sliced the string properly to grab the correct information.

--- Lesson 4 Strings/Lab 4 In-Class/lab4.py
+++ Lesson 4 Strings/Lab 4 In-Class/lab4.py
@@ -3,69 +3,95 @@ import login
 
 def main():
     info = ["first_name", "last_name"]
+    spelling = ["first name", "last name"]
     print(len(info))
     print(info[0])
+    i = 0
 
     for names in info:
         # First Name
         start = "first"
-        info[names] = input('enter your first name: ').split()
+        names = input('enter your %s: ' % (spelling[i])).split()
         while True:
             if start == "first":
-                info[names] = ''.join(map(str, info[names]))
-                info[names] = info[names].title()
-                if info[names].isalpha():
+                names = ''.join(map(str, names))
+                names = names.title()
+                if names.isalpha():
                     break
-                elif len(info[names].split()) > 1:
+                elif len(names.split()) > 1:
                     start = "incorrect spacing"
                     continue
                 else:
                     start = "incorrect"
                     continue
             elif start == "incorrect":
-                info[names] = input('Please enter only alphabetical characters for your first name: ').split()
-                while len(info[names]) > 1:
-                    info[names] = input('Please enter only one word: ').split()
+                names = input('Please enter only alphabetical characters for your %s: ' % (spelling[i])).split()
+                while len(names) > 1:
+                    names = input('Please enter only one word: ').split()
                     continue
-                info[names] = ''.join(map(str, info[names]))
-                info[names] = info[names].title()
-                if info[names].isalpha():
+                names = ''.join(map(str, names))
+                names = names.title()
+                if names.isalpha():
                     break
-                elif len(info[names].split()) > 1:
+                elif len(names.split()) > 1:
                     start = "incorrect spacing"
                     continue
                 else:
                     start = "incorrect"
                     continue
             elif start == "incorrect spacing":
-                info[names] = input('There was incorrect spacing. We only need your first name: ').split()
-                while len(info[names]) > 1:
-                    info[names] = input('Please enter only one word: ').split()
+                names = input('There was incorrect spacing. We only need your %s: ' % (spelling[i])).split()
+                while len(names) > 1:
+                    names = input('Please enter only one word: ').split()
                     continue
-                info[names] = ''.join(map(str, info[names]))
-                info[names] = info[names].title()
-                if info[names].isalpha():
+                names = ''.join(map(str, names))
+                names = names.title()
+                if names.isalpha():
                     break
                 else:
                     start = "incorrect"
 
             elif start == "correct":
-                info[names] = input('enter your first name: ').split()
-                info[names] = ''.join(map(str, info[names]))
-                info[names] = info[names].title()
-                if info[names].isalpha():
+                names = input('enter your %s: ' % (spelling[i])).split()
+                names = ''.join(map(str, names))
+                names = names.title()
+                if names.isalpha():
                     break
 
-            print(type(info[names]))
-            print(start)
+        if info[i] == "first_name":
+            first_name = names.title()
+            print(first_name)
+        elif info[i] == "last_name":
+            last_name = names.title()
+            print(last_name)
 
-        print(info[0])
+        i += 1
 
+    print(len(first_name))
+    print(len(last_name))
 
+    if len(first_name) > 3:
+        first_name_grabbed = first_name[0:3]
+        print(first_name_grabbed)
+    else:
+        print(first_name)
 
+    if len(last_name) > 3:
+        last_name_grabbed = last_name[0:3]
+        print(last_name_grabbed)
+    else:
+        print(last_name)
 
+    bcit_id = input(" enter your BCIT ID: ")
+    print(bcit_id)
+    length = len(bcit_id)
+    if length >= 3:
+        bcit_id_grabbed = bcit_id[length - 3:]
+        print(bcit_id_grabbed)
+
+    concatenated_characters = first_name_grabbed + last_name_grabbed + bcit_id_grabbed
+    print(concatenated_characters)
 
 
 if __name__ == "__main__":
     main()
-
