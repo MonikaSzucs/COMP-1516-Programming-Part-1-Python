a22ee3786d3589dd137271048547775d7e3cdaa9
Monika <monika.silvia.s@hotmail.ca>
2021-10-12 13:05:09 -0700

    Started Lab 5 for countries

---
+++ Lesson 5 Lists/Lesson5Lab/Lesson5Lab.py
@@ -0,0 +1,16 @@
+"""
+A script with three tuples and the main function
+:author: Monika Szucs
+:date: October 13 2021
+"""
+import information as info
+
+
+def main():
+    info.how_many_countries()
+    info.get_name_of_longest_country()
+    info.get_number_of_capitals_containing()
+
+
+if __name__ == "__main__":
+    main()
