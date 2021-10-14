c1457393d2284aebb0908301d5ccdeabc179b873
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-13 22:41:03 -0700

    Started assignment 1

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -2,25 +2,40 @@
 Assignment 1 by Monika Szucs, A00878763
 """
 from data import countries_and_capitals, countries, capitals
+import json
 
 
 def print_json_countries_and_capitals():
-    pass
+    print(countries)
+    x = {}
+    increment = 0
+    while True:
+        if increment < len(countries):
+            x.update({"country_name": countries[increment],
+                      "capital_city": capitals[increment]
+                      })
+        else:
+            break
+        increment += 1
+    print(x)
+
+
 
 
 def get_list_of_countries_whose_nth_letter_is(n,letter):
-
+    pass
 
 
 def get_funny_case_capital_cities(letter):
+    pass
 
 
 def get_doubled_letter_countries():
-
+    pass
 
 
 def main():
-    pass
+    print("I should not be called")
 
 
 if __name__ == "__main__":
--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -5,11 +5,10 @@ import assignment1
 
 
 def main():
-    print_json_countries_and_capitals()
-    print(get_list_of_countries_whose_nth_letter_is(n,letter))
-    print(get_funny_case_capital_cities(letter))
-    print(get_doubled_letter_countries())
-    pass
+    print(assignment1.print_json_countries_and_capitals())
+    #print(get_list_of_countries_whose_nth_letter_is(n,letter))
+    #print(get_funny_case_capital_cities(letter))
+    #print(get_doubled_letter_countries())
 
 
 if __name__ == "__main__":
