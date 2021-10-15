ce1a38907b797763daa88d598f3c357b01e233b0
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-14 21:16:33 -0700

    Did more of Assignment 1
    Started on get_funny_case_capital_cities but still not done yet.  Must start here.

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -6,28 +6,47 @@ import json
 
 
 def print_json_countries_and_capitals():
-    print(countries)
-    x = {}
     increment = 0
+    total = []
     while True:
         if increment < len(countries):
-            x.update({"country_name": countries[increment],
-                      "capital_city": capitals[increment]
-                      })
+            pair = {"country_name": countries[increment],
+                    "capital_city": capitals[increment]
+                    }
+            total.append(pair)
         else:
             break
         increment += 1
-    print(x)
+    print("print_json_countries_and_capitals")
+    print(total)
 
 
-
-
-def get_list_of_countries_whose_nth_letter_is(n,letter):
-    pass
+def get_list_of_countries_whose_nth_letter_is(n, letter):
+    position = n - 1
+    number = 0
+    answer = []
+    for country in countries:
+        if letter == countries[number][position]:
+            answer.append(countries[number])
+        number += 1
+    print(answer)
 
 
 def get_funny_case_capital_cities(letter):
-    pass
+    print(letter)
+    print(capitals)
+    position = 0
+    number = 0
+    answer = []
+    for capital in capitals:
+        print(capital)
+        print(capital[number])
+        if letter == capitals[number][position]:
+            answer.append(capitals[number])
+        number += 1
+    print(answer)
+
+
 
 
 def get_doubled_letter_countries():
--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -6,8 +6,8 @@ import assignment1
 
 def main():
     print(assignment1.print_json_countries_and_capitals())
-    #print(get_list_of_countries_whose_nth_letter_is(n,letter))
-    #print(get_funny_case_capital_cities(letter))
+    print(assignment1.get_list_of_countries_whose_nth_letter_is(3,"m"))
+    print(assignment1.get_funny_case_capital_cities("u"))
     #print(get_doubled_letter_countries())
 
 
