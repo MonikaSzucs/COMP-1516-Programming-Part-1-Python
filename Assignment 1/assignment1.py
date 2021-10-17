35f615f641907aad2d4285bf227728d4ff7fb440
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-16 23:26:32 -0700

    Still working on 2nd last function
    Not completed yet.

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -37,25 +37,57 @@ def get_funny_case_capital_cities(letter):
     print("START")
     print(letter)
     print(capitals)
+    position_within = 0
+    position = 0
+    letter_position = 0
+    position_within_list = list()
+    for capital in capitals:
+        print(capital)
+        position_within_list.append(capital)
+        print(position_within_list)
+        print(position_within_list[position])
+        print(len(position_within_list[position]))
+        while position_within < len(position_within_list[position]):
+            print("inside while loop")
+            print(position_within)
+            print(position_within_list[position])
+            print(position_within_list[position][letter_position])
+            if letter == position_within_list[position][letter_position].lower():
+                print("inside if statement")
+                print(letter)
+                print(position_within)
+                position_within_list[position][letter_position - 1].upper()
+                print(position_within_list[position][letter_position-1].upper())
+                
+
+            position_within += 1
+            letter_position += 1
+    print(position_within_list)
+
+    """
     answer = []
     working_on = ''
     for capital in capitals:
         position_within = 0
+        position_within_list = ()
         print(capital)
         print(capital[position_within])
         while position_within < len(capital):
             print(position_within)
             if letter == capital[position_within].lower():
-                print("IT IS MATCHING" + capital[position_within].lower())
+                print("IT IS MATCHING " + capital[position_within].lower())
                 working_on += capital
                 print(working_on)
                 working_on = capital[:position_within - 1].lower() + capital[position_within - 1:].capitalize()
-
+                working_on = capital[:position_within + 1].lower() + capital[position_within + 1:].capitalize()
+                position_within += 1
                 print(working_on)
 
             position_within += 1
+            
+    """
     print("get_funny_case_capital_cities:")
-    print(answer)
+    #print(answer)
 
 
 
