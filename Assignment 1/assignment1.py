0a08cd08fdf780a3c7319d1cdd526b4b6fbdeb14
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-22 23:28:54 -0700

    Figured out more of the Assignment 1
    still not done function but very close. Just need to grab additional values that can be found in the same word.

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -37,7 +37,6 @@ def get_funny_case_capital_cities(letter):
     print("\n")
     print("get_funny_case_capital_cities:")
     case_capitals = []
-
     for capital in capitals:
         letter_position = []
         print(capital)
@@ -46,39 +45,67 @@ def get_funny_case_capital_cities(letter):
             print(capital[i])
             if capital[i].lower() == letter.lower():
                 letter_position.append(i)
+        print("Letter position here for all found")
         print(letter_position)
         single_capital = []
         print("second for loop")
         pos = 0
-        for j in range(len(capital)):
-            print(j)
-            print(letter_position)
-            print(capital[j])
-            if j > 0:
-                if len(capital) < (j + 1):
-                    print(capital[j].lower())
-                    single_capital.append(capital[j].lower())
-            pos += 1
+        if len(letter_position) == 0:
+            continue
+        else:
+            skipped = 0
+            number_position = 0
+            for j in range(len(capital)):
+                if skipped == 0:
+                    print("start for loop")
+                    print(len(capital))
+                    print(j)
+                    print(letter_position)
+                    #print(letter_position[number_position])
+                    print(capital[j])
+                    if j > 0:
+                        print("number position of letter that is matched")
+                        print(letter_position[number_position])
+                        if j == letter_position[number_position]:
+                            print("inside if")
+                            print(letter_position[number_position])
+                            single_capital.pop()
+                            single_capital.append(capital[j-1].upper())
+                            print(capital[j].lower())
+                            single_capital.append(capital[j].lower())
+                            if j < len(capital):
+                                print("inside if statement")
+                                print(len(capital))
+                                print(j)
+                                if len(capital) - 1 == j:
+                                    print("last item in list")
+                                else:
+                                    single_capital.append(capital[j+1].upper())
+                                    print(single_capital)
+                                    skipped = 1
+                            else:
+                                print("inside else statmeent inside second loop")
+                                continue
+                        elif j == len(capital) - 1:
+                            print("last letter")
+                            single_capital.append(capital[j].lower())
+                        else:
+                            single_capital.append(capital[j].lower())
+                    elif j == 0:
+                        single_capital.append(capital[j].lower())
+                        print("initial position")
+                    else:
+                        single_capital.append(capital[j].lower())
+                    pos += 1
+                else:
+                    skipped = 0
+                    continue
         print(single_capital)
-
-
-
-        # for character in capital:
-        """    print(character)
-            print(capital[character_count])
-            if character.lower() == letter.lower():
-                number += 1
-                letter.upper()
-                if number == 1:
-                    case_capitals.append(capital)
-            cap += 1
-            character_count += 1
-        print(cap)
-        print(len(capital))
-        if cap >= len(capital):
-            print("END")
-        print(case_capitals)"""
-
+        single_capital = ''.join(single_capital)
+        print(single_capital)
+        case_capitals.append(single_capital)
+        print("case_capitals")
+        print(case_capitals)
 
 
 
