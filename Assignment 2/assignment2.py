71a1aadd9227ce45eae5caa6a8cbc0f557256720
Monika <monika.silvia.s@hotmail.ca>
2021-10-26 13:29:40 -0700

    Set up all of Assignment 2
    The basic template is set up to start doing assignment 2

--- Assignment 2/assignment2.py
+++ Assignment 2/assignment2.py
@@ -1,3 +1,24 @@
+from data import countries_and_capitals, countries, capitals, countries_capitals_dictionary
+
+
+def write_countries_capitals_to_file(file):
+    # must contain only one letter or digits
+    # Must be of length 1-8 characters, plus a ".txt" file extension
+    # If file name not valid then ask the user for another filename. This must continue over and over until they put a
+    # proper file name. Use Regular Expression to validate the filename
+    print("IN HERE")
+
+
+def save_capitals():
+    #opens files for writing only
+    #1 vowel_vowel_vowel.text
+    #2 Cons_cons_cons.txt
+    #3 i_before_e.txt
+    #4 a_a.txt
+    #5 end_with_vowel. txt
+    #6 weird.txt
+    #7 not_start.txt
+    print("CHECKING")
 
 
 def main():
