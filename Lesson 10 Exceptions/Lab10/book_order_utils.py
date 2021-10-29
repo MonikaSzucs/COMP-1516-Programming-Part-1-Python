8fbcb69d778f52d55ffda8b82766623aada8c3c7
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-10-28 21:55:00 -0700

    Setup Lab10

---
+++ Lesson 10 Exceptions/Lab10/book_order_utils.py
@@ -0,0 +1,13 @@
+import re
+
+
+def validate_book_order_details(order_num, title, author, isbn, year, quantity, cost):
+    pass
+
+
+def calculate_per_book_cost(cost, quantity):
+    pass
+
+
+def write_book_order_details(filename, title, author, isbn, year, quantity, cost, unit_cost):
+    pass
---
+++ Lesson 10 Exceptions/Lab10/main.py
@@ -0,0 +1,9 @@
+from book_order_utils import validate_book_order_details
+
+
+def main():
+    validate_book_order_details()
+
+
+if __name__ == "__main__":
+    main()
