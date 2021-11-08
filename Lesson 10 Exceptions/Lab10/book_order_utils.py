a70ffd221064ef7ce26df9168e72b654d1d7c864
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-07 23:14:37 -0800

    Counted leading zeros
    Finished counting leading zeros

--- Lesson 10 Exceptions/Lab10/book_order_utils.py
+++ Lesson 10 Exceptions/Lab10/book_order_utils.py
@@ -2,7 +2,24 @@ import re
 
 
 def validate_book_order_details(order_num, title, author, isbn, year, quantity, cost):
-    pass
+    print(order_num)
+    print(int(order_num))
+    order_num_array = []
+    order_num_array.append(order_num)
+    removed_leading_zeros = [ele.lstrip('0') for ele in order_num_array]
+    print(str(removed_leading_zeros))
+    print(len(removed_leading_zeros[0]))
+    print("---")
+    leading_zeros_amount = 0
+    while True:
+        if len(removed_leading_zeros[0]) == leading_zeros_amount:
+            print("equals")
+            break
+        else:
+            leading_zeros_amount += 1
+    print("done")
+    print(leading_zeros_amount)
+    print("---")
 
 
 def calculate_per_book_cost(cost, quantity):
--- Lesson 10 Exceptions/Lab10/main.py
+++ Lesson 10 Exceptions/Lab10/main.py
@@ -2,7 +2,7 @@ from book_order_utils import validate_book_order_details
 
 
 def main():
-    validate_book_order_details()
+    validate_book_order_details("0021", "The title here", "author", "12345458346", "2012", "10", "20.0")
 
 
 if __name__ == "__main__":
