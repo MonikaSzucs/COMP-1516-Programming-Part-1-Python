584e0589756f4c2ec94403a607a5b2588b2a6f87
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-13 20:37:22 -0800

    Finished Lab 11

--- Lesson 11 OOP/Lab11/car.py
+++ Lesson 11 OOP/Lab11/car.py
@@ -1,8 +1,25 @@
+"""
+Lab 11 by Monika Szucs A00878763
+A script containing the class Car with def __init__, calc_profit(), get_details(), reduce_price()
+:author: Monika Szucs
+:date: November 13, 2021
+"""
+
 class Car:
     """ Represents a car in a car lot """
-
     def __init__(self, make, model, year, cost, price):
         """ Initializes the car details """
+        """
+            A function that will grab the values entered in by the user and organize it into its self container to be 
+            used later
+            First Variant and parameters
+            :param self: set up OOP to call values later within each function
+            :param make: Contains the make of the car
+            :param model: Contains the Model name of the car
+            :param year: Contains the year of when the car was built
+            :param cost: Contains the cost of the car
+            :param price: Contains the initial price of the car
+        """
         self._make = make
         self._model = model
         self._year = year
@@ -11,15 +28,33 @@ class Car:
 
     def calc_profit(self):
         """ Returns the projected profit """
+        """
+            A function that will return the projected profit
+            Second Variant, parameter, and return
+            :param self: contains all the values of the car entered in by the user 
+            :return: the projected profit of the car
+        """
         return self._price - self._cost
 
     def get_details(self):
-        # TODO
+        """
+            A function that will grab the details of the car and present it in a nicely formatted sentence
+            Third Variant, parameter, and return
+            :param self: contains all the values of the car entered in by the user
+            :return: the details of the car in a formatted sentence
+        """
         print(type(self._price))
         return "%s %s %s for sale for $%.2f" % (self._year, self._make, self._model, float(self._price))
 
     def reduce_price(self, reduction):
-        # TODO
+        """
+            A function that will grab the details of the car and then calculate the profit minus the reduction
+            to return the reduced price
+            Fourth Variant, parameters, and return
+            :param self: contains all the values of the car entered in by the user
+            :param reduction: contains the reduced amount
+            :return: the reduced price
+        """
         reduced_price = self.calc_profit() - reduction
         return reduced_price
 
--- Lesson 11 OOP/Lab11/main.py
+++ Lesson 11 OOP/Lab11/main.py
@@ -1,8 +1,16 @@
+"""
+Lab 11 by Monika Szucs A00878763
+A script containing the main() working function
+:author: Monika Szucs
+:date: November 13, 2021
+"""
+
+# importing the file Car and its functions with OOP
 from car import Car
 
 
 def main():
-	car0 = Car("Honda", "Civic", 2020, 15000.0, 9999.99)
+	car0 = Car("Honda", "Civic", 2015, 15000.0, 9999.99)
 	print(car0.get_details())
 	print()
 
@@ -14,8 +22,8 @@ def main():
 
 	car2 = Car("BMW", "M3", 2019, 30000, 50000)
 	print(car2.get_details())
-	print("First Car’s Initial Profit is $%.2f" % car2.calc_profit())
-	print("First Car’s New Profit is $%.2f" % car2.reduce_price(5000.50))
+	print("Second Car’s Initial Profit is $%.2f" % car2.calc_profit())
+	print("Second Car’s New Profit is $%.2f" % car2.reduce_price(5000.50))
 
 
 if __name__ == '__main__':
