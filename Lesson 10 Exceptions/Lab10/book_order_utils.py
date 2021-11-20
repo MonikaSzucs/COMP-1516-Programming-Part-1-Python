29a183b5ad3b8cc61c2ab89cb0d2fe2a0a8bc086
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-19 19:38:58 -0800

    finished Part A - Validated_book_order_details and calculate_perbook_cost

--- Lesson 10 Exceptions/Lab10/book_order_utils.py
+++ Lesson 10 Exceptions/Lab10/book_order_utils.py
@@ -10,88 +10,66 @@ import re
 
 def validate_book_order_details(order_num, title, author, isbn, year, quantity, cost):
     try:
-        print(order_num)
-        print(int(order_num))
-        order_num_array = [order_num]
-        removed_leading_zeros = [ele.lstrip('0') for ele in order_num_array]
-        print(str(removed_leading_zeros))
-        print(len(removed_leading_zeros[0]))
-        print(order_num_array)
         print("--Order Number-")
-        leading_zeros_amount = 0
-        while True:
-            if len(removed_leading_zeros[0]) == leading_zeros_amount:
-                break
+        order_num_converted = int(order_num)
+        if type(order_num_converted) == int:
+            order_num_array = [order_num]
+            removed_leading_zeros = [ele.lstrip('0') for ele in order_num_array]
+            leading_zeros_amount = 0
+            while True:
+                if len(removed_leading_zeros[0]) == leading_zeros_amount:
+                    break
+                else:
+                    leading_zeros_amount += 1
+            print(leading_zeros_amount)
+            print("--Title--")
+            if re.search("^[a-zA-Z\s]+$", title):
+                print(title)
+                print("--author--")
+                if re.search("^[a-zA-Z\s\'\"]+$", author):
+                    print(author)
+                    print("--ISBN--")
+                    if re.search("^[0-9]{4,20}$", isbn):
+                        print(isbn)
+                        print("--Year--")
+                        if re.search("^[0-9]{4}$", year):
+                            print(year)
+                            print("--Quantity--")
+                            if re.search("^([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|1000)$", quantity):
+                                print(quantity)
+                                print("--Cost--")
+                                if re.search("^\d+\.\d{2}$", cost):
+                                    print(cost)
+                                else:
+                                    raise ValueError("cost must be a floating point value with exactly 2 decimal places")
+                            else:
+                                if (len(quantity) <= 0 or len(quantity) >= 4) and type(quantity) != int:
+                                    raise ValueError("quantity must be between 0 and 1000, inclusive")
+                                raise TypeError("quantity must be an integer")
+                        else:
+                            if len(year) > 4 or len(year) < 4:
+                                raise ValueError("year must be 4 digits exactly.")
+                            raise TypeError("year must be a integer.")
+                    else:
+                        raise TypeError("ISBN must be an integer and between 4 and 20 digits, inclusive")
+                else:
+                    raise ValueError("Author is invalid")
             else:
-                leading_zeros_amount += 1
-        print(leading_zeros_amount)
-    except ValueError:
-        print("Order Number is invalid")
-    try:
-        print("--Title--")
-        if re.search("^[a-zA-Z\s]+$", title):
-            print(title)
+                raise ValueError("There is no lower, uppercase letter(s) or spaces")
         else:
-            print("Title is invalid")
-    except (ValueError, TypeError):
-        print("Title is invalid")
-    try:
-        print("--author--")
-        if re.search("^[a-zA-Z\s\'\"]+$", author):
-            print(author)
-        else:
-            print("Author is invalid")
-    except (ValueError, TypeError):
-        print("Author is invalid.")
-    try:
-        print("--ISBN--")
-        if re.search("^[0-9]{4,20}$", isbn):
-            print(isbn)
-        else:
-            print("ISBN must be an integer")
-    except (ValueError, TypeError):
-        print("ISBN must be an integer.")
-    try:
-        print("--Year--")
-        print(int(year))
-        if re.search("^[0-9]{4}$", year):
-            print(year)
-        else:
-            print("Year is invalid")
-    except (ValueError, TypeError):
-        print("Year must be an integer.")
-    try:
-        print("--Quantity--")
-        print(int(quantity))
-        if re.search("^[0-9]{0,1000}$", quantity):
-            print(quantity)
-        else:
-            print("Quantity is invalid")
-    except (ValueError, TypeError):
-        print("Quantity must be an integer.")
-    try:
-        print("--Cost--")
-        print(cost)
-        print(float(cost))
-        if re.search("^\d+\.\d{2}$", cost):
-            print(cost)
-        else:
-            print("Cost is invalid")
-    except (ValueError, TypeError):
-        print("Cost is invalid.")
-    print()
+            raise ValueError('invalid order number')
+    except ValueError as e:
+        print("The ValueError is " + str(e))
+    except TypeError as e:
+        print("The TypeError is " + str(e))
 
 
 def calculate_per_book_cost(cost, quantity):
-    print(cost)
-    print(quantity)
     try:
-        cost_per_book = cost/quantity
-        print(cost_per_book)
+        cost_per_book = cost / quantity
         return cost_per_book
     except ZeroDivisionError:
         return "The quantity cannot be zero"
-    print()
 
 
 def write_book_order_details(filename, title, author, isbn, year, quantity, cost, unit_cost):
@@ -111,4 +89,3 @@ def write_book_order_details(filename, title, author, isbn, year, quantity, cost
             break
         except FileExistsError:
             filename = input("Please enter another file name because it has already been used: ")
-
--- Lesson 10 Exceptions/Lab10/main.py
+++ Lesson 10 Exceptions/Lab10/main.py
@@ -5,14 +5,15 @@ A script containing the main() function
 :date: November 15, 2021
 """
 
-from book_order_utils2 import validate_book_order_details, calculate_per_book_cost, write_book_order_details
+from book_order_utils import validate_book_order_details, calculate_per_book_cost, write_book_order_details
 
 
 def main():
-    validate_book_order_details("0021", "The title here", "Monika Szucs", "12345458346", "2012", "10", "20.0")
-    #validate_book_order_details("0021", "The Title Here", "Monika's24", "12345458346", "2012", "10", 20.01)
-    #print(calculate_per_book_cost(12.00, 2))
-    #print(calculate_per_book_cost(12.00, 0))
+    validate_book_order_details("0021", "The title here", "Monika Szucs", "123432432", "2012", "1000", "20.00")
+    print()
+    print(calculate_per_book_cost(12.00, 2))
+    print()
+    print(calculate_per_book_cost(12.00, 0))
     #filename = input("Please enter a file name: ")
     #write_book_order_details(filename, "Intro to Python", "Bill Smith", 123456, 2010, 10, 500.50, 50.05)
 
