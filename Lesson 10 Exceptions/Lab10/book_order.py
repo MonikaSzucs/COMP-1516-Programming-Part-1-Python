e76c94eda84b94d087d7248d2211ab8ebde6fa7c
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-21 09:54:07 -0800

    Started Part B of lab 10

--- Lesson 10 Exceptions/Lab10/book_order.py
+++ Lesson 10 Exceptions/Lab10/book_order.py
@@ -6,13 +6,30 @@ A script containing the main() function
 """
 
 import sys
-
-
-def main():
-    print("main")
-    print("number of arguments", len(sys,argv), 'arguments.')
-
+import argparse
 
 if __name__ == "__main__":
-    main()
+    # initialize the parser
+    parser = argparse.ArgumentParser(
+        description="my mathscript"
+    )
 
+    # add the parameters positional/optional
+    parser.add_argument('-n', '--num1', help="Number 1", type=float)
+    parser.add_argument('-i', '--num2', help="Number 2", type=float)
+    parser.add_argument('-o', '--operation', help="provide operator", default="+")
+
+    # Parse the arguments
+    args = parser.parse_args()
+    print(args)
+    result =  None
+    if args.operation == '+':
+        result = args.num1 + args.num2
+    if args.operation == '-':
+        result = args.num1 - args.num2
+    if args.operation == '*':
+        result = args.num1 * args.num2
+    if args.operation == 'pow':
+        result = pow(args.num1, args.num2)
+
+    print("Result:",  result)
