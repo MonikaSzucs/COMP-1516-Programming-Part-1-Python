1fb569f969cc53724e5bb70ef0b740ed005c232a
Monika <monika.silvia.s@hotmail.ca>
2021-11-22 15:06:36 -0800

    Did more of Lab 10
    Help clarified a bit from the teacher but still do not understand the rest of the project.

--- Lesson 10 Exceptions/Lab10/book_order.py
+++ Lesson 10 Exceptions/Lab10/book_order.py
@@ -6,74 +6,17 @@ A script containing the main() function
 """
 
 import sys
-import os
-from aifc import Error
 from book_order_utils import validate_book_order_details, calculate_per_book_cost, write_book_order_details
-from inspect import signature
-
-
-#def check_data(param):
-    # isinstance()
-    # if not type(param) is str:
-    #    raise TypeError("Parameter must be a string")
-#    if not isinstance(param, str):
-#        raise TypeError("Parameter must be a string")
-
-
-#def raise_error():
-#    value = int(input("Please enter a number: "))
-#
-#    if value < 0:
-#        raise ValueError("Cannot have a negative number")
 
 
 def main():
     print(sys.argv)
-    print(sys.argv[0])
-    sig = signature(validate_book_order_details)
-    print(type(sig))
-    print(len(sig))
-    print(str(sig))
-    print(type(str(sig)))
-    print(repr(sig))
-    print(len(repr(sig)))
-    # print(len(sys.argv))
-    # num = 0
-    # while num < len(sys.argv):
-    #    print(sys.argv[num])
-    #    num += 1
-
-    # print(os.getenv())
-    # print(os.getcwd())
-    # path = os.getcwd()
-    # print(os.path.isfile(path))
-    # print(os.path.exists(path))
-    # filename = "test.txt"
-    # print(os.path.isfile(filename))
-    # print(os.path.exists(filename))
-    # full_path = os.path.join(path, filename)
-    # print(full_path)
-
-    """
-    try:
-        # check_data(145)
-        check_data("Hello")
-        raise_error()
-    except TypeError as t:
-        print(t)
-    except ValueError as v:
-        print(v)
-    except Error as e:
-        print("Didn't  handle the specific error and now we are here")
-        print(e)
-
-    while True:
-        try:
-            raise_error()
-            break
-        except ValueError as v:
-            print(v)
-    """
+    length_argument = len(sys.argv) - 1
+    start = 1
+    print()
+    while start <= length_argument:
+        print(sys.argv[start])
+        start += 1
 
 
 if __name__ == '__main__':
