110a2eb7e9f6407c5c37113377f3fec449b45b82
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-23 20:45:38 -0800

    Testing

--- Testing Folder/main.py
+++ Testing Folder/main.py
@@ -1,11 +1,17 @@
+import re
+
+
+def get_input():
+    while True:
+        data = input("Please Enter a Four Digit Integer: ")
+        if re.search("^\d{4}$", data) is None:
+            print("Input must be a four digit integer")
+        else:
+            return data
 
 
 def main():
-    with open('books.txt') as file:
-        lines = [line.rstrip() for line in file]
-    for i, word in enumerate(lines):
-        lines[i] = word.upper()
-    print(lines)
+    print(get_input())
 
 
 if __name__ == "__main__":
