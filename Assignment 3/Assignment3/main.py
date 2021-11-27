83f35f13468a42ba22ce33e5de7ab4e76031aea0
Monika Szucs <monika.silvia.s@hotmail.ca>
2021-11-26 22:32:20 -0800

    Working on Assignment 3

--- Assignment 3/Assignment3/main.py
+++ Assignment 3/Assignment3/main.py
@@ -6,12 +6,23 @@ A script containing the main() function
 """
 from Country import Country
 from data import countries_and_capitals
+import random
 
 
 def main():
     try:
-        country = Country("Austria", "Vienna", 1000)
-        print(country.print_details())
+        all_countries = []
+        for country in countries_and_capitals:
+            print(country)
+            #random_value = random.randint(1000000, 1000000000)
+            random_value = random.randint(1000000, 2000000)
+            all_countries.append([country[0], country[1], random_value])
+            country = Country(country[0], country[1], random_value)
+            print(country.print_details())
+            #input("Press enter to check the next country")
+        print(all_countries)
+
+
     except ValueError as v:
         print(str(v))
 
